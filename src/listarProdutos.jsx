dc1626eabade841ed66efd1fcb69565f973770a9
priscila204 <priscila.silva12@estudante.ifms.edu.br>
2024-08-25 18:59:49 -0400

    25/08/24
    trabalho cardápio continuação

---
+++ src/listarProdutos.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+export default function Home() {
+    const [listaProdutos, setPtrodutos] = useState([
+    { id: 1, nome: 'Produto A', preco: '55.00'},
+    { id: 2, nome: 'Produto B', preco: '40.00'},
+    { id: 3, nome: 'Produto C', preco: '85.00'},
+    { id: 4, nome: 'Produto D', preco: '59.90'},
+    { id: 5, nome: 'Produto E', preco: '75.00'},
+    { id: 6, nome: 'Produto F', preco: '29.99'},
+    { id: 7, nome: 'Produto G', preco: '55.00'},
+    { id: 9, nome: 'Produto H', preco: '55.00'},
+    { id: 10, nome: 'Produto I', preco: '55.00'},
+
+    ]);
+
+const [listaPedidos, setListaPedidos] = useState([]);
+const adicionarItemPedidos = (objeto) => {
+    setListaPedidos([...listaPedidos,objeto])
+}
+return (
+    <div>
+        <h1>Minha Loja Virtual</h1>
+        <div>
+            {listaProdutos.map((item) => (
+                <div key={item.id}>
+                    {item.nome} - R$ {item.preco}
+                    <button onClick={() => adicionarItemPedidos(item)}>Adicionar ao Pedido</button>
+                </div>
+            ))}
+        </div>
+        
+    </div>
+ );
+}
