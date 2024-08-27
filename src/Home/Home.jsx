a255baa19e0dfbb3635f71fc6bcbe543bcf139cd
priscila204 <priscila.silva12@estudante.ifms.edu.br>
2024-08-27 10:23:33 -0400

    27/08
    continuação trabalho ptac

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -21,21 +21,21 @@ export default function Home() {
             imagem: "https://m.media-amazon.com/images/I/91RpwagB7uL._AC_UF894,1000_QL80_.jpg"
         },
         {
-            id: 3,
+            id: 4,
             nome: "Attack on Titan",
             preco: 27.90,
             cor: "colorido",
             capitulo: 139,
             imagem: "https://mangalivre.com.br/wp-content/uploads/2021/11/Attack-on-Titan-4-GQCA-img-qSKtAl-847x1200.jpeg"
         },
-        {
-            id: 4,
-            nome: "My Hero Academia",
-            preco: 26.90,
-            cor: "colorido",
-            capitulo: 386,
-            imagem: "https://www.google.com/url?sa=i&url=https%3A%2F%2Feditorajbc.com.br%2Fmangas%2Fcolecao%2Fmy-hero-academia%2F&psig=AOvVaw1MRkwGQxQzBpSziMR88xf1&ust=1724806066729000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCKjy8735k4gDFQAAAAAdAAAAABAE"
-        },
+            {
+                id: 3,
+                nome: "Attack on Titan",
+                preco: 27.90,
+                cor: "colorido",
+                capitulo: 139,
+                imagem: "https://mangalivre.com.br/wp-content/uploads/2021/11/Attack-on-Titan-4-GQCA-img-qSKtAl-847x1200.jpeg"
+            },
         {
             id: 5,
             nome: "Demon Slayer",
