61b0f4163ab3ac2e6d089d766432bf1fa50a673a
priscila204 <priscila.silva12@estudante.ifms.edu.br>
2024-08-27 22:25:31 -0400

    27/08
    trabalho PTAC.... fnalização apos o prazo de envio.

--- src/Componentes/listarProdutos.jsx
+++ src/Componentes/listarProdutos.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react';
 
 export default function Cardapio({ itens }) {
     const [listaProdutos, setProdutos] = useState(itens);
-
     const [listaPedidos, setListaPedidos] = useState([]);
 
     const adicionarItemPedidos = (objeto) => {
@@ -23,25 +22,28 @@ export default function Cardapio({ itens }) {
     }
 
     return (
-        <div>
-            <div>
+        <div className="container">
+            <div className="product-list">
                 {listaProdutos.map((item) => (
-                    <div key={item.id}>
-                        <img src={item.imagem}></img>
-                        {item.nome} - R$ {item.preco}
-                        <button onClick={() => adicionarItemPedidos(item)}>Adicionar ao Pedido</button>
+                    <div key={item.id} className="product-card">
+                        <img src={item.imagem} className="product-image" alt={item.nome} />
+                        <div className="product-info">
+                            <p>{item.nome} - R$ {item.preco}</p>
+                            <button onClick={() => adicionarItemPedidos(item)} className="add-button">Adicionar ao Pedido</button>
+                        </div>
                     </div>
                 ))}
             </div>
-            <div>
+            <div className="order-list">
                 {listaPedidos.map((produto) => (
-                    <div key={produto.id}>
+                    <div key={produto.id} className="order-card">
                         <p>{produto.nome}</p>
                         <p>R$ {produto.preco}</p>
-                        <button onClick={() => removerPedido(produto.id)}>Remover</button>
+                        <button onClick={() => removerPedido(produto.id)} className="remove-button">Remover</button>
                     </div>
                 ))}
             </div>
         </div>
     );
 }
+
